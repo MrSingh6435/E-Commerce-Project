81bce7834e00492b7cb68c6e9143c5aa70cab319
TejayPardeep <109805068+TejayPardeep@users.noreply.github.com>
2024-03-03 21:28:10 +0530

    New update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+// Menue button click
 let menu = document.querySelector("#menu");
-menu.addEventListener('click', ()=>{
-    console.log("Menu button is clicked");
+let menu1 = document.querySelector("#menu1");
+let closeMenu = document.querySelector("#close");
+let sideBar = document.querySelector("#sideBar");
+
+menu.addEventListener('click', () => {
+    sideBar.classList.add('active');
+})
+menu1.addEventListener('click', () => {
+    sideBar.classList.add('active');
+})
+closeMenu.addEventListener('click', () => {
+    sideBar.classList.remove('active');
 })
 
 // Select Lanugage function
@@ -8,4 +19,12 @@ const languageSelector = document.getElementById('language');
 languageSelector.addEventListener('change', function () {
     const selectedLanguage = languageSelector.value;
     console.log('Selected language:', selectedLanguage);
+});
+
+// Search Bar Logic to apply border
+document.querySelector('#searchBar').addEventListener('focus', () => {
+    document.querySelector('#navSearch').classList.add('active');
+});
+document.querySelector('#searchBar').addEventListener('blur', () => {
+    document.querySelector('#navSearch').classList.remove('active');
 });
